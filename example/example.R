cd07f6ed012c1364248e04ead8d59b40be637b48
Trang Le <grixor@gmail.com>
2020-04-29 14:45:24 -0400

    add example

---
+++ example/example.R
@@ -0,0 +1,15 @@
+library(treeheatr)
+library(tidyverse)
+
+diabetes <- read_csv('diabetes.csv') %>% 
+  rename('Diabetes status' = Outcome)
+
+heat_tree(
+  data = diabetes,
+  target_lab = 'Diabetes status',
+  label_map = c(`0` = 'Negative', `1` = 'Positive'),
+  target_cols = NULL
+) %>% 
+  ggsave(filename = 'diabetes.png', .,
+         height = 3.5, width = 7)
+ 
